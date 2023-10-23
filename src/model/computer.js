dd86314413d371e9b68cfa8412f661ab2b02439b
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-10-23 23:24:52 +0900

    fix: 임의의 정답을 생성하는 함수

--- src/model/computer.js
+++ src/model/computer.js
@@ -1,10 +1,7 @@
-import { BALL_SIZE } from "../utils/constant";
+import { Random } from "@woowacourse/mission-utils";
 import { addRandomNumber } from "../utils/validity";
 
 class Computer {
-  constructor() {
-    this.size = BALL_SIZE;
-  }
 
   // 임의의 정답을 생성해주는 함수
   generateRandomNumber() {
@@ -12,5 +9,16 @@ class Computer {
     while(ANSWER.length < size) {
       addRandomNumber(ANSWER)
     }
+    return ANSWER.join('');
   }
+
+  // 임의의 정답 생성 시 각 자릿수의 값이 겹치지 않는지 확인해주는 함수
+  addRandomNumber(answer) {
+    const NUMBER = Random.pickNumberInRange(1, 9);
+    if (!answer.includes(NUMBER)) {  
+      answer.push(NUMBER);
+    }
+  };
 }
+
+export default Computer;
