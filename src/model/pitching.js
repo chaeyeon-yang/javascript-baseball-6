d41e64eb7fa69b642c06c76dc2a6013b083e7213
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-10-24 01:45:45 +0900

    refactor: 공을 비교해주는 함수 리팩토링

--- src/model/pitching.js
+++ src/model/pitching.js
@@ -1,11 +1,17 @@
 import { BALL_SIZE } from '../utils/constant'
+
 class Pitching {
-  
+
+  #computerBall
+  constructor(randomNumber) {
+    this.#computerBall = randomNumber;
+  }
+
   // 스트라이크 개수를 반환하는 함수
-  countStrike(userBall, computerBall) {
+  countStrike(userBall) {
     const STRIKE_COUNT = 0;
     for(let i = 0; i < BALL_SIZE; i++) {
-      if (userBall[i] === computerBall[i]) {
+      if (userBall[i] === this.#computerBall[i]) {
         STRIKE_COUNT += 1;
       }
     }
@@ -13,10 +19,10 @@ class Pitching {
   }
 
   // 볼 개수를 반환하는 함수
-  countBall(userBall, computerBall) {
+  countBall(userBall) {
     const BALL_COUNT = 0;
     for (let i = 0; i < BALL_SIZE; i++) {
-      if (computerBall.includes(userBall[i])) {
+      if (this.#computerBall.includes(userBall[i])) {
         BALL_COUNT += 1;
     }
     return BALL_COUNT;
@@ -24,11 +30,8 @@ class Pitching {
   }
 
   // 낫싱 여부를 판단하는 함수
-  isNothing(userBall, computerBall) {
-    if (!countStrike(userBall, computerBall) && !countBall(userBall, computerBall)) {
-      return true;
-    }
-    return false;
+  isNothing(userBall) {
+    return !countStrike(userBall) || !countBall(userBall) 
   }
 }
 
