4ef99740bdec19f9018250cb86c50ed9ddb78226
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-10-25 21:57:39 +0900

    feat: 사용자가 입력한 볼을 정답과 비교하는 로직

--- src/controller/gameController.js
+++ src/controller/gameController.js
@@ -1,5 +1,9 @@
 import { Console } from "@woowacourse/mission-utils";
+import constant from "../utils/constant";
 import InputView from "../view/inputView";
+import OutputView from "../view/outputView";
+import Pitching from "../model/pitching";
+import Computer from "../model/computer";
 
 class GameController {
 
@@ -8,18 +12,38 @@ class GameController {
   #computerBall
 
   #computer
+  #pitching
   #inputView
+  #outputView
   constructor() {
     this.#computer = new Computer();
+    this.#pitching = new Pitching();
     this.#inputView = new InputView();
+    this.#outputView = new OutputView();
   }
 
   // 숫자 야구 시작
   gameStart() {
-    Console.log(constant.GAME_MESSAGE.start);
-    this.#userBall = this.#inputView.getUserInput()
+    Console.print(constant.GAME_MESSAGE.start);
     this.#computerBall = this.#computer.generateRandomNumber();
     this.gameProgress();
+  }
+
+  // 숫자 야구 진행
+  gameProgress() {
+    while (true) {
+      this.#userBall = this.#inputView.getUserInput();
+      const strikeBall = this.#pitching.countStrike(this.#userBall);
+      const ballBall = this.#pitching.countBall(this.#userBall);
+
+      this.#outputView.judgeBall(strikeBall, ballBall);
+
+      if (judgeSuccess(strikeBall)) {
+        Console.print(constant.GAME_MESSAGE.success)
+        this.gameEndOrRestart();
+        break;
+      }
+    }
   }
 }
 
